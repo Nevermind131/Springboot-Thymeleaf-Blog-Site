6dac7fe78e45c7bd0ae45e67b815d75a42918984
Nevermind131 <84068912+Nevermind131@users.noreply.github.com>
2022-08-16 12:05:07 +0800

    上传了sql文件

---
+++ blog.sql
@@ -0,0 +1,118 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 8.0.28 : Database - myblog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`myblog` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `myblog`;
+
+/*Table structure for table `blog` */
+
+DROP TABLE IF EXISTS `blog`;
+
+CREATE TABLE `blog` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID编号',
+  `title` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '博客标题',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '博客摘要',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '博客内容',
+  `create_time` datetime NOT NULL COMMENT '发布时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `type_id` bigint NOT NULL COMMENT '所属专栏',
+  `views` bigint unsigned DEFAULT '0' COMMENT '浏览量',
+  `recommend` tinyint(1) DEFAULT '0' COMMENT '是否开启推荐',
+  `share_statement` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否开启转载声明',
+  `commentabled` tinyint(1) DEFAULT '0' COMMENT '是否开启评论',
+  `flag` int DEFAULT '1' COMMENT '1.原创；2.转载；3.翻译',
+  `published` tinyint(1) DEFAULT '0' COMMENT 'false:草稿；true:已发布',
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`,`title`) USING BTREE,
+  UNIQUE KEY `t_u` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=139 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `blog_tags` */
+
+DROP TABLE IF EXISTS `blog_tags`;
+
+CREATE TABLE `blog_tags` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `blog_id` bigint NOT NULL,
+  `tag_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `blog_tags_ibfk_1` (`blog_id`),
+  KEY `blog_tags_ibfk_2` (`tag_id`),
+  CONSTRAINT `blog_tags_ibfk_1` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `blog_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=313 DEFAULT CHARSET=utf8mb3;
+
+/*Table structure for table `comment` */
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID编号',
+  `nickname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发表评论的用户名称',
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '邮箱地址',
+  `blog_id` bigint NOT NULL COMMENT '博客ID编号',
+  `create_time` datetime NOT NULL COMMENT '发布时间',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '评论内容',
+  `parent_id` bigint DEFAULT '-1' COMMENT '父评论id',
+  `parent_comment` json DEFAULT NULL COMMENT '父评论',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `reply_comments` json DEFAULT NULL COMMENT '子评论',
+  `admin_comment` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`,`blog_id`) USING BTREE,
+  KEY `foreign_blog_id` (`blog_id`),
+  CONSTRAINT `foreign_blog_id` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `tag` */
+
+DROP TABLE IF EXISTS `tag`;
+
+CREATE TABLE `tag` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '标签id编号',
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '标签名称',
+  `number` bigint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `type` */
+
+DROP TABLE IF EXISTS `type`;
+
+CREATE TABLE `type` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID编号',
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '专栏名称',
+  `number` bigint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) DEFAULT NULL,
+  `username` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员账号',
+  `password` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员密码',
+  `email` varchar(50) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `introduction` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
